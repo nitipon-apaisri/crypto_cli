64f1d7268b568dc3dc84849f0e4c9dd7ebd3427e
nitipon <nitipon.apaisri@gmail.com>
2023-04-24 16:04:50 +0200

    Added inteval update and some styles

--- bin/index.js
+++ bin/index.js
@@ -3,26 +3,30 @@ import chalk from "chalk";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import boxen from "boxen";
-import Table from "cli-table";
+import Table from "cli-table3";
 const log = console.log;
 const table = new Table({
-    head: ["Currency", "Price", "24 hrs Change(%)"],
-    colWidths: [30, 50, 50],
+    head: [chalk.cyan.bold("Currency"), chalk.cyan.bold("Price"), chalk.cyan.bold("24 hrs Change(%)")],
+    colWidths: [20, 20, 20],
 });
+
 const getCoins = async () => {
     const response = await fetch("http://localhost:4200/api/coins");
     const data = await response.json();
     return data.data;
 };
-const coins = await getCoins();
-table.push(
-    [coins.coins[0].name, `${Number(coins.coins[0].price).toFixed(2)} USD`, coins.coins[0].change],
-    [coins.coins[1].name, `${Number(coins.coins[1].price).toFixed(2)} USD`, coins.coins[1].change]
-);
-if (hideBin(process.argv).length === 0) log(boxen("Welcome to My CLI", { padding: 1, margin: 1, borderStyle: "round" }));
-yargs(hideBin(process.argv))
-    .command("coins", "show top 10 coins", () => log(table.toString()))
-    .demandCommand(1)
-    .parse();
-// log(chalk.blue("Hello world!"));
-// console.log("Hello world!");
+const updateTable = async () => {
+    const coins = await getCoins();
+    if (table.length !== 0) table.splice(0, table.length);
+    console.clear();
+    await coins.coins.forEach((coin) => {
+        table.push([coin.name, `${Number(coin.price).toFixed(2)} USD`, coin.change.includes("-") ? chalk.red(coin.change) : chalk.green(coin.change)]);
+    });
+    if (hideBin(process.argv).length === 0) log(boxen("Welcome to My CLI", { padding: 1, margin: 1, borderStyle: "round" }));
+    yargs(hideBin(process.argv))
+        .command("coins", "show top 10 coins", () => log(table.toString()))
+        .demandCommand(1)
+        .parse();
+};
+await updateTable();
+setInterval(updateTable, 5000);
