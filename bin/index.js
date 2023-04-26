a57e3e32ae780d4545cdfbcf9260430a804f5942
nitipon <nitipon.apaisri@gmail.com>
2023-04-26 22:00:03 +0200

    Able to check crypto price bt ids

--- bin/index.js
+++ bin/index.js
@@ -16,33 +16,51 @@ const getCoins = async () => {
     return data.data;
 };
 const getCoinById = async (coin) => {
-    const response = await fetch(`https://crypto-cli.onrender.com/api/coins/${coin.toLowerCase()}`);
+    const response = await fetch(`https://crypto-cli.onrender.com/api/coin/${coin}`);
     const data = await response.json();
     return data.data;
 };
-const updateTable = async () => {
-    const coins = await getCoins();
+const getCoinByIds = async (coins) => {
+    const response = await fetch(`https://crypto-cli.onrender.com/api/coins/${coins}`);
+    const data = await response.json();
+    return data.data;
+};
+const updateTable = async (data) => {
     if (table.length !== 0) table.splice(0, table.length);
     console.clear();
-    await coins.data.forEach((coin) => {
+    if (typeof data === Array) {
+        data.data.forEach((coin) => {
+            table.push([
+                coin.name,
+                `${Number(coin.priceUsd).toFixed(2)} USD`,
+                coin.changePercent24Hr.includes("-") ? chalk.red(Number(coin.changePercent24Hr).toFixed(2)) : chalk.green(Number(coin.changePercent24Hr).toFixed(2)),
+            ]);
+        });
+    } else {
         table.push([
-            coin.name,
-            `${Number(coin.priceUsd).toFixed(2)} USD`,
-            coin.changePercent24Hr.includes("-") ? chalk.red(Number(coin.changePercent24Hr).toFixed(2)) : chalk.green(Number(coin.changePercent24Hr).toFixed(2)),
+            data.data.name,
+            `${Number(data.data.priceUsd).toFixed(2)} USD`,
+            data.data.changePercent24Hr.includes("-") ? chalk.red(Number(data.data.changePercent24Hr).toFixed(2)) : chalk.green(Number(data.data.changePercent24Hr).toFixed(2)),
         ]);
-    });
+    }
     log(table.toString());
 };
 if (hideBin(process.argv).length === 0) log(boxen("Welcome to My CLI", { padding: 1, margin: 1, borderStyle: "round" }));
 yargs(hideBin(process.argv))
     .command("coins", "show top 10 coins", async () => {
-        await updateTable();
-        setInterval(updateTable, 5000);
+        await updateTable(await getCoins());
+        setInterval(async () => updateTable(await getCoins()), 5000);
     })
-    .command("coin <coin>", "show coin by name e.g Bitcoin", async (yargs) => {
+    .command("coins <coin>", "show coin(s) by name(s) e.g Bitcoin Ethereum", async (yargs) => {
         try {
-            const coin = await getCoinById(yargs.argv._[1]);
-            console.log(coin);
+            const coins = yargs.argv._.slice(1);
+            if (coins.length > 1) {
+                await updateTable(await getCoinByIds(coins.toString()));
+                setInterval(async () => updateTable(await getCoinByIds(coins)), 5000);
+            } else {
+                await updateTable(await getCoinById(coins[0]));
+                setInterval(async () => updateTable(await getCoinById(coins[0])), 5000);
+            }
         } catch (error) {
             console.log(error);
         }
